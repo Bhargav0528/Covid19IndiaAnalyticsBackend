685726c5e94cc910bd39d6473c874a4ce70621da
bharg <bhargavauden1998@gmail.com>
2020-03-28 04:01:04 +0530

    covid 19 analytics requirements

--- covid19Controller.py
+++ covid19Controller.py
@@ -7,8 +7,8 @@ api = Api(app)
 
 class AgeCalculator(Resource):
     def get(self):
-        startAge = int(request.args.get('startAge'))
-        endAge = int(request.args.get('endAge'))
+        startAge =  request.args.get('startAge')
+        endAge = request.args.get('endAge')
         return {'startAge': startAge, 'endAge': endAge}
 
 api.add_resource(AgeCalculator, '/calculateAge')
